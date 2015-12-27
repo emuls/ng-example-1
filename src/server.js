6dddd4b672debe58c35c46c6384e4b4e7ba41671
Rex Jolley <emulsifier@gmail.com>
2015-12-27 12:53:07 -0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.use('/', express.static('www'));
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Basic node server initialized with "' + dataFile + '" listening at http://%s:%s', host, port);
+});
