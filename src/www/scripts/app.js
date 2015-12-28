548a577fb9b8ec7c1a9c107208d81998f091d5ee
Rex Jolley <emulsifier@gmail.com>
2015-12-27 23:49:01 -0700

    adding parse example

--- src/www/scripts/app.js
+++ src/www/scripts/app.js
@@ -1,11 +1,21 @@
-angular.module('helloApp', []).controller('HelloController', function($scope, $timeout){
+angular.module('helloApp', []).controller('HelloController', function($scope, $timeout, $parse){
     $scope.clock = {};
     $scope.name = 'World';
+    $scope.expression = '$scope.name';
+    $scope.parsedExpression = '';
     var updateClock = function(){
         $scope.clock.now = new Date();
         $timeout(function(){
             updateClock();
         }, 1000);
     }
+
+    $scope.$watch('expression', function(newVal, oldVal, scope){
+       if(newVal!==oldVal){
+           var parseFunction = $parse(newVal);
+           $scope.parsedExpression = parseFunction(scope);
+       }
+    });
+
     updateClock();
 });
