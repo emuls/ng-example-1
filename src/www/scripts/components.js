6be54c37457a06c3b2434a5f6f15ecc85745413e
Rex Jolley <emulsifier@gmail.com>
2016-01-12 21:51:25 -0700

    converting components to use anonymous functions

--- src/www/scripts/components.js
+++ src/www/scripts/components.js
@@ -1,34 +1,41 @@
-
-angular.module('exampleApp').directive('exampleCard', function(){
-   return {
-       restrict: 'E',
-       scope:{
-           title:'@'
-       },
-       templateUrl:'/components/directives/examplecard.view.html',
-       transclude:true
-   }
-});
-
-angular.module('exampleApp').directive('carBlurb', function(){
-    return {
-        restrict: 'E',
-        replace: false,
-        scope:{
-            car:'='
-        },
-        templateUrl:'/components/directives/carblurb.view.html'
+!(function() {
+    function ExampleCardDirective() {
+        return {
+            restrict: 'E',
+            scope: {
+                title: '@'
+            },
+            templateUrl: '/components/directives/examplecard.view.html',
+            transclude: true
+        }
     }
-});
 
-angular.module('exampleApp').directive('rainbowDate', function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: {
-            date: '=date'
-        },
-        template: '<div class="rainbowDate">' +
+    angular.module('exampleApp').directive('exampleCard', ExampleCardDirective);
+})();
+
+!(function() {
+    function CarBlurbDirective() {
+        return {
+            restrict: 'E',
+            replace: false,
+            scope: {
+                car: '='
+            },
+            templateUrl: '/components/directives/carblurb.view.html'
+        }
+    }
+    angular.module('exampleApp').directive('carBlurb', CarBlurbDirective);
+})();
+
+!(function(){
+    function RainbowDateDirective(){
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                date: '=date'
+            },
+            template: '<div class="rainbowDate">' +
             '<span class="month">{{date.getMonth()+1 | numberFixedLen:2}}</span>/' +
             '<span class="day">{{date.getDate()}}</span>/' +
             '<span class="year">{{date.getFullYear()}}</span>' +
@@ -36,116 +43,153 @@ angular.module('exampleApp').directive('rainbowDate', function(){
             '<span class="hours">{{date.getHours() | numberFixedLen:2}}</span>:' +
             '<span class="minutes">{{date.getMinutes() | numberFixedLen:2}}</span>:' +
             '<span class="seconds">{{date.getSeconds() | numberFixedLen:2}}</span>' +
-        '</div>'
-    }
-});
-
-angular.module('exampleApp').directive('makeGreen', function(){
-    return {
-        restrict: 'A',
-        replace: false,
-        priority: 99,
-        terminal: true,
-        compile: function(element, attrs, fTransclude){
-            element.css({"color":"green"})
+            '</div>'
         }
     }
-});
+    angular.module('exampleApp').directive('rainbowDate', RainbowDateDirective);
+})();
 
-angular.module('exampleApp').directive('makeRed', function(){
-    return {
-        restrict: 'A',
-        replace: false,
-        priority: 100,
-        terminal: true,
-        compile: function(element, attrs, fTransclude){
-            element.css({"color":"red"})
+!(function() {
+    'use strict';
+    function MakeGreenDirective() {
+        return {
+            restrict: 'A',
+            replace: false,
+            priority: 99,
+            terminal: true,
+            compile: compile
         }
     }
-});
 
-angular.module('exampleApp').directive('transcludeBlue', function(){
-    return {
-        restrict: 'EA',
-        priority: 101,
-        replace:false,
-        scope: {}, //must be isolated, {}, or set to true
-        transclude:true,
-        template:'<div style="color:blue" ng-transclude></div>'
+    function compile(element, attrs, fTransclude) {
+        element.css({"color": "green"})
     }
-});
 
-angular.module('exampleApp').directive('contestExample', function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl:'/components/directives/clickcontestexample.view.html',
-        controllerAs:'contest',
-        controller:function($timeout){
-            var self = this;
-            this.rolling=false;
-            this.dice=4;
-            this.reroll = function(){
-                this.rolling = true;
-                $timeout(function(){
-                    self.rolling = false;
-                    self.dice = self.RNG(1,6);
-                }, 1500)
-            };
+    angular.module('exampleApp').directive('makeGreen', MakeGreenDirective);
+})();
 
-            this.RNG = function(min, max) {
-                return Math.random() * (max - min) + min;
-            };
+
+!(function() {
+    'use strict';
+    function MakeRedDirective() {
+        return {
+            restrict: 'A',
+            replace: false,
+            priority: 99,
+            terminal: true,
+            compile: compile
         }
     }
-});
 
-angular.module('exampleApp').directive('trueScope', function(){
-    return{
-        restrict: 'E',
-        scope:true,
-        template:'<div>' +
-        '<p>This directive has a scope:true property and inherits it\'s scope prototypically as a result</p>' +
-        '<h4>{{controllerProperty}}</h4>' +
-        '<h4>{{controllerPropertyTwo}}</h4>' +
-        '</div>'
-    };
-});
+    function compile(element, attrs, fTransclude) {
+        element.css({"color": "red"})
+    }
 
-angular.module('exampleApp').directive('noScope', function(){
-    return{
-        restrict: 'E',
-        template:'<div>' +
-        '<p>This directive does not have a scope property and inherits prototypically as a result</p>' +
-        '<h4>{{controllerProperty}}</h4>' +
-        '<h4>{{controllerPropertyTwo}}</h4>' +
-        '</div>'
-    };
-});
+    angular.module('exampleApp').directive('makeRed', MakeRedDirective);
+})();
 
-angular.module('exampleApp').directive('isolateScope', function(){
-    return{
-        restrict: 'E',
-        scope:{
-            controllerPropertyTwo: '='
-        },
-        template:'<div>' +
+!(function(){
+    function TranscludeBlueDirective(){
+        return {
+            restrict: 'EA',
+            priority: 101,
+            replace:false,
+            scope: {}, //must be isolated, {}, or set to true
+            transclude:true,
+            template:'<div style="color:blue" ng-transclude></div>'
+        }
+    }
+    angular.module('exampleApp').directive('transcludeBlue', TranscludeBlueDirective);
+})();
+
+!(function() {
+    function ContestExampleDirective() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/components/directives/clickcontestexample.view.html',
+            controllerAs: 'contest',
+            controller:ContestExampleController
+        }
+    }
+
+    function ContestExampleController($timeout) {
+        var self = this;
+        this.rolling = false;
+        this.dice = 4;
+        this.reroll = function () {
+            this.rolling = true;
+            $timeout(function () {
+                self.rolling = false;
+                self.dice = self.RNG(1, 6);
+            }, 1000)
+        };
+
+        this.RNG = function (min, max) {
+            return Math.random() * (max - min) + min;
+        };
+    }
+    angular.module('exampleApp').directive('contestExample', ContestExampleDirective);
+})();
+
+!(function(){
+
+    function TrueScopeDirective(){
+        return{
+            restrict: 'E',
+            scope:true,
+            template:'<div>' +
+            '<p>This directive has a scope:true property and inherits it\'s scope prototypically as a result</p>' +
+            '<h4>{{controllerProperty}}</h4>' +
+            '<h4>{{controllerPropertyTwo}}</h4>' +
+            '</div>'
+        };
+    }
+    angular.module('exampleApp').directive('trueScope',TrueScopeDirective);
+})();
+
+!(function(){
+    function NoScopeDirective(){
+        return{
+            restrict: 'E',
+            template:'<div>' +
+            '<p>This directive does not have a scope property and inherits prototypically as a result</p>' +
+            '<h4>{{controllerProperty}}</h4>' +
+            '<h4>{{controllerPropertyTwo}}</h4>' +
+            '</div>'
+        };
+    }
+    angular.module('exampleApp').directive('noScope', NoScopeDirective);
+})();
+
+!(function(){
+    function IsolateScopeDirective(){
+        return{
+            restrict: 'E',
+            scope:{
+                controllerPropertyTwo: '='
+            },
+            template:'<div>' +
             '<p>This directive sets it\'s scope property and thus does not inherit, the example controller property one should not appear below.  However, it double-binds property two using \'=\' notation</p>' +
             '<h4>{{controllerProperty}}</h4>' +
             '<h4>{{controllerPropertyTwo}}</h4>' +
-        '</div>'
-    };
-});
-
-angular.module('exampleApp').controller('ScopeExampleController', function($scope){
-    $scope.controllerProperty = 'Controller Property';
-    this.propState = true;
-    $scope.changePropTwo = function(){
-        this.propState = !this.propState;
-        $scope.controllerPropertyTwo = this.propState ? 'Controller Property Two!' : 'Controller Property Two?';
+            '</div>'
+        };
     }
+    angular.module('exampleApp').directive('isolateScope',IsolateScopeDirective);
+})();
 
-    $scope.changePropTwo();
+!(function(){
+    function ScopeExampleController($scope){
+        $scope.controllerProperty = 'Controller Property';
+        this.propState = true;
+        $scope.changePropTwo = function(){
+            this.propState = !this.propState;
+            $scope.controllerPropertyTwo = this.propState ? 'Controller Property Two!' : 'Controller Property Two?';
+        }
 
-});
+        $scope.changePropTwo();
+    }
+    angular.module('exampleApp').controller('ScopeExampleController',ScopeExampleController);
+})();
 
