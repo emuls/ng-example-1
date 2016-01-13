e1f3194928844d10a5ff60e44fdfd94b76981c8f
Rex Jolley <emulsifier@gmail.com>
2016-01-12 21:31:06 -0700

    converting app.js to use anonymous functions

--- src/www/scripts/app.js
+++ src/www/scripts/app.js
@@ -1,56 +1,76 @@
 angular.module('exampleApp', ['ngMaterial','ngRoute','services']);
 
-angular.module('exampleApp').controller('AppController', function($scope, $mdSidenav,$mdMedia, TitleService){
-    $scope.$mdMedia = $mdMedia;
-    $scope.TitleService = TitleService;
-    $scope.toggleNav = function (){
-        $mdSidenav('leftnav').toggle();
-    }
-});
-
-angular.module('exampleApp').directive('navigation', function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: '/components/navigation/navigation.view.html',
-        controllerAs:'nav',
-        controller: function(){
-            this.links = [
-                {
-                    id: 0,
-                    text: 'Basic Examples',
-                    route: '/example'
-                },
-                {
-                    id: 1,
-                    text: 'Form Example',
-                    route: '/formexample'
-                },
-                {
-                    id: 2,
-                    text: 'API Example',
-                    route: '/apiexample'
-                }
-            ]
+!(function(){
+    'use strict';
+    function AppController($scope, $mdSidenav, $mdMedia, TitleService){
+        $scope.$mdMedia = $mdMedia;
+        $scope.TitleService = TitleService;
+        $scope.toggleNav = function (){
+            $mdSidenav('leftnav').toggle();
         }
     }
-});
+    angular.module('exampleApp').controller('AppController', AppController);
+})();
 
-angular.module('exampleApp').config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('altTheme').primaryPalette('red')
-    $mdThemingProvider.setDefaultTheme('altTheme');
-});
+!(function(){
+    'use strict';
+    function themeConfig($mdThemingProvider){
+        $mdThemingProvider.theme('altTheme').primaryPalette('red')
+        $mdThemingProvider.setDefaultTheme('altTheme');
+    }
+    angular.module('exampleApp').config(themeConfig);
+})();
 
-angular.module('exampleApp').config(function($routeProvider) {
-    $routeProvider
-        .when('/example', {
-            template: '<basic-examples></basic-examples>',
-        }).when('/formexample', {
+
+!(function(){
+    'use strict';
+    function routeConfig($routeProvider){
+        $routeProvider
+            .when('/example', {
+                template: '<basic-examples></basic-examples>',
+            }).when('/formexample', {
             template: '<form-example></form-example>'
         }).when('/apiexample', {
             template: '<api-call-example></api-call-example>'
         }).otherwise({
-                redirectTo: '/example'
+            redirectTo: '/example'
         });
-});
+    }
+    angular.module('exampleApp').config(routeConfig);
+})();
+
+!(function(){
+    'use strict';
+     function navigationDirective(){
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/components/navigation/navigation.view.html',
+            controllerAs:'nav',
+            controller: function(){
+                this.links = [
+                    {
+                        id: 0,
+                        text: 'Basic Examples',
+                        route: '/example'
+                    },
+                    {
+                        id: 1,
+                        text: 'Form Example',
+                        route: '/formexample'
+                    },
+                    {
+                        id: 2,
+                        text: 'API Example',
+                        route: '/apiexample'
+                    }
+                ]
+            }
+        }
+    }
+
+    angular.module('exampleApp').directive('navigation', navigationDirective);
+})();
+
+
 
