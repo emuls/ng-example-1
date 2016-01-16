3ac2f6a1e07baa2a724de1ffba15197c60ef9b28
Rex Jolley <emulsifier@gmail.com>
2016-01-15 19:33:07 -0700

    adding ngannotate and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ng-annotate');
 
     grunt.initConfig({
         watch: {
@@ -38,8 +40,33 @@ module.exports = function (grunt) {
                 }
             },
         },
+        ngAnnotate : {
+            options: {
+                singleQuotes: true,
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['DIST/scripts/main.js'],
+                        ext: '.annotated.js', // Dest filepaths will have this extension.
+                        extDot: 'last',       // Extensions in filenames begin after the last dot
+                    },
+                ],
+            },
+        },
+        uglify: {
+            options: {
+                mangle: true
+            },
+            min: {
+                files: {
+                    'DIST/scripts/main.min.js': ['DIST/scripts/main.annotated.js']
+                }
+            }
+        }
     });
      // the default task (running "grunt" in console) is "watch"
      grunt.registerTask('default', ['watch']);
-     grunt.registerTask('build', ['clean','copy:htmldist','concat:dist','less:dist']);
+     grunt.registerTask('build', ['clean','copy:htmldist','concat:dist','less:dist','ngAnnotate','uglify']);
 };
