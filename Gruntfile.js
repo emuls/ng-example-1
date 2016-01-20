f04c8a005b587e0c7e3e1d8d69da72b1dc14b860
Rex Jolley <rex.jolley@cmcflex.com>
2016-01-20 14:16:15 -0700

    fixing less task

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,9 +31,6 @@ module.exports = function(grunt) {
 		},
 		less: {
 			dist: {
-				options: {
-					paths: ["stylesheets/"],
-				},
 				files: {
 					"DIST/stylesheets/style.css": "src/**/*.less"
 				}
